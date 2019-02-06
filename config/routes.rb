f0d10c126ff056cd8849aeff4d908ba9cc4431af
solaretech <solaretech.kk@gmail.com>
2019-02-06 05:55:39 +0000

    [Create]Routing & Devise Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root 'users#top'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only:[:show, :index, :edit, :update]
+  resources :articles, only:[:show, :index, :new, :edit, :create, :update, :destroy] do
+    resource :stories, only:[:new, :create, :destroy]
+  end
+  resources :stories, only:[:show, :new, :edit, :create, :update, :destroy]
+  resources :article_histories, only:[:index, :show, :create]
+  resources :libraries, only:[:index, :create, :destroy]
+
+  patch 'users/d/:id' => 'users#unsubscribe', as: 'unsubscribe_user'
 end
