6d1240b06a4b3c480810bca0d713254041682ffa
solaretech <solaretech.kk@gmail.com>
2019-02-06 05:35:39 +0000

    [Create]Controller files

---
+++ app/controllers/article_histories_controller.rb
@@ -0,0 +1,2 @@
+class ArticleHistoriesController < ApplicationController
+end
---
+++ test/controllers/article_histories_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ArticleHistoriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
