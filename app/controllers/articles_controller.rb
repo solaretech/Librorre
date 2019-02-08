11f061ca6ef989bd7639a94bb87d2a9ab272cb4f
solaretech <solaretech.kk@gmail.com>
2019-02-08 13:43:11 +0000

    [Update]エラー記事編集・閲覧画面

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,24 @@ class ArticlesController < ApplicationController
     @categories = Category.all
   end
 
+  def edit
+    @article = Article.find(params[:id])
+    @category_list = @article.categories.pluck(:name).join(",")
+  end
+
   def create
     article = Article.new(article_params)
     article.user_id = current_user.id
     category_list = params[:category_list].split(",")
     article.save
+    article.save_categories(category_list)
+    redirect_to article_path(article)
+  end
+
+  def update
+    article= Article.find(params[:id])
+    category_list = params[:category_list].split(",")
+    article.update(article_params)
     article.save_categories(category_list)
     redirect_to article_path(article)
   end
