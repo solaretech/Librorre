5d995fc88d93feba5cec07dd1547bcb1cb5bb32d
solaretech <solaretech.kk@gmail.com>
2019-02-17 09:08:59 +0000

    [Fix]ストーリー追加・編集画面のcocoon不具合修正

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,10 +7,11 @@ class StoriesController < ApplicationController
   end
 
   def show
-    @story = Story.find(params[:id])  # 選択したストーリー
-    @article = @story.article         # 選択したストーリーに関連するエラー記事
-    @stories = Story.page(params[:page]).per(10)       # 同一エラーに関するストーリー
-    @topics = @story.story_topics
+    @story = Story.find(params[:id])              # 選択したストーリー
+    @article = @story.article                     # 選択したストーリーに関連するエラー記事
+    @stories = Story.page(params[:page]).per(10)  # 同一エラーに関するストーリー
+    @topics = @story.story_topics                 # 見出し
+    logging_visited_stories(@story)               # 閲覧履歴の記録
   end
 
   def edit
@@ -30,10 +31,17 @@ class StoriesController < ApplicationController
 
   def update
     story = Story.find(params[:id])
-    category_list = params[:category_list].split(",")
     story.update(story_params)
+    category_list = params[:category_list].split(",")
     story.save_story_categories(category_list)
     redirect_to story_path(story)
+  end
+
+  def destroy
+    story = Story.find(params[:id])
+    article = story.article
+    story.destroy
+    redirect_to article_path(article)
   end
 
   private
@@ -42,9 +50,11 @@ class StoriesController < ApplicationController
     params.require(:story).permit(
       :title,
         story_topics_attributes: [
+          :id,
           :title,
           :_destroy,
           story_bodies_attributes: [
+            :id,
             :content,
             :text_type,
             :_destroy
