8d3b84941b359d87bd1da9d0f57fb7e701c38070
solaretech <solaretech.kk@gmail.com>
2019-02-20 06:30:01 +0000

    [Fix]カテゴリのkaminariページング設定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.order(:created_at).reverse_order.page(params[:page]).per(10)
+    @stories = @category.stories.order(:created_at).reverse_order.page(params[:page]).per(10)
   end
 end
