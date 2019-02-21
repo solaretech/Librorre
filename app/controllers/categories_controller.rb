5806586664ec300a6e6dfa9113f03ed9d268a0cd
solaretech <solaretech.kk@gmail.com>
2019-02-21 08:46:09 +0000

    [Fix]タグ検索時のもっと見る機能の修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,12 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @articles = @category.articles.order(:created_at).reverse_order.page(params[:page]).per(10)
     @stories = @category.stories.order(:created_at).reverse_order.page(params[:page]).per(10)
+    return unless request.xhr?
+    case params[:type]
+    when 'article'
+      render "/articles/article_list"
+    when 'story'
+      render "/stories/story_list"
+    end
   end
 end
