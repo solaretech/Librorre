88ba1892aa3f53bc15d90b85d4576358cc2acb44
solaretech <solaretech.kk@gmail.com>
2019-02-23 14:12:37 +0000

    [Fix]レイアウト調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,8 +43,9 @@ $(document).on('turbolinks:load', function() {
   });
 });
 
-// コンテンツの高さ調整
 $(document).on('turbolinks:load', function() {
+
+  // コンテンツの高さ調整
   function elementSet(idName){
     box = $('#content-'+idName).find('.content-box');
     head = $('#content-'+idName).find('.content-box-head');
@@ -66,15 +67,15 @@ $(document).on('turbolinks:load', function() {
     elementSet('left');
     elementSet('right');
   });
-});
 
-// レスポンシブ表示時のスイッチング
-$(document).on('turbolinks:load', function() {
+  // レスポンシブ表示時のスイッチング
   // 左右それぞれのcontent-boxにあるヘッダーテキストを取得
   var leftText = $('#content-left').find('.content-box-head').text();
+  var leftSubText = $('#sub-content').find('.content-box-head').text();
+  var leftMainText = leftText.replace(leftSubText, '');
   var rightText = $('#content-right').find('.content-box-head').text();
   // 左右それぞれにコンテンツがあるか確認。
-  if(rightText != null){
+  if(rightText != ""){
     // 左右それぞれのフッター部分に要素追加。まずは要素自体を取得。
     var leftBox = $('#content-left');
     var rightBox = $('#content-right');
@@ -84,18 +85,19 @@ $(document).on('turbolinks:load', function() {
     rightBox.append('<div id="switch-left"></div>');
     //挿入したHTML要素にテキストを表示
     $('#switch-right').html(rightText + '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>');
-    $('#switch-left').html('<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>' + leftText);
-
-    //左右切り替えスイッチ
-    $('#switch-right').click(function(){
-      $('#content-left').hide();
-      $('#content-right').show();
-    });
-    $('#switch-left').click(function(){
-      $('#content-right').hide();
-      $('#content-left').show();
-    });
+    $('#switch-left').html('<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>' + leftMainText);
   }
+  //左右切り替えスイッチ
+  $('#switch-right').click(function(){
+    $('#content-left').hide();
+    $('#content-right').show();
+    elementSet('right');
+  });
+  $('#switch-left').click(function(){
+    $('#content-right').hide();
+    $('#content-left').show();
+    elementSet('left');
+  });
 
 });
 
