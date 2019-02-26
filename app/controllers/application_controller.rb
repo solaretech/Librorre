e0e36af9c1c6bd096320d2b03c01bf543b4aaef6
solaretech <solaretech.kk@gmail.com>
2019-02-26 06:56:28 +0000

    [Fix]ApplicationController アクション記載順の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,29 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # 作成者・管理者以外のアクセスを制限
+  # User#update
+  def ensure_authority
+    unless params[:id] == current_user.id
+      unless current_user.admin = true
+        redirect_to user_path(current_user), alert: '許可されないリクエストです。'
+      end
+    end
+  end
+
+  # 管理者ログイン認証
+  def auth_admin
+    if user_signed_in?
+      user = User.find(current_user.id)
+      if user.admin == false
+        redirect_to root_path, alert: '許可されていないリクエストです。'
+      end
+    else
+      redirect_to root_path, alert: '許可されていないリクエストです。'
+    end
+  end
+
+  # 編集履歴の作成
   def create_article_history(old_article)
     @new_history = ArticleHistory.new
     @new_history.article_id = old_article.id
@@ -9,6 +32,15 @@ class ApplicationController < ActionController::Base
     @new_history.mean = old_article.mean
     @new_history.cause = old_article.cause
     @new_history.save
+  end
+
+  # 閲覧履歴が10件を超えた場合、古い閲覧履歴を削除
+  def visited_stories_over_ten_pages?
+    visits_stock_limit = 10
+    @visited_stories = current_user.visiteds.reverse
+    if @visited_stories.count > visits_stock_limit
+      @visited_stories[visits_stock_limit-1].destroy
+    end
   end
 
   # ユーザーがストーリーページを表示した時、そのページの情報をVisitedモデルに保存
@@ -31,36 +63,6 @@ class ApplicationController < ActionController::Base
     if Visited.exists?(story_id: "#{params[:id]}")
       @old_visited = Visited.find_by(story_id: "#{params[:id]}")
       @old_visited.destroy
-    end
-  end
-
-  # 閲覧履歴が10件を超えた場合、古い閲覧履歴を削除
-  def visited_stories_over_ten_pages?
-    @visited_stories = current_user.visiteds.reverse
-    if @visited_stories.count > 10
-      @visited_stories[9].destroy
-    end
-  end
-
-  # 作成者・管理者以外のアクセスを制限
-  # User#update
-  def ensure_authority
-    unless params[:id] == current_user.id
-      unless current_user.admin = true
-        redirect_to user_path(current_user), alert: '許可されないリクエストです。'
-      end
-    end
-  end
-
-  # 管理者ログイン認証
-  def auth_admin
-    if user_signed_in?
-      user = User.find(current_user.id)
-      if user.admin == false
-        redirect_to root_path, alert: '許可されていないリクエストです。'
-      end
-    else
-      redirect_to root_path, alert: '許可されていないリクエストです。'
     end
   end
 
